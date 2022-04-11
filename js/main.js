7aeac0c2a07a1941c1a82236c7305193ccea9053
cold-miss <inceptionrose@gmail.com>
2022-04-11 16:01:52 +0900

    main

--- js/main.js
+++ js/main.js
@@ -122,5 +122,25 @@ $(function(){
         }
     });
 
+
+    // aboutme parallax scroll
+
+    let bg = document.getElementById("bg")
+    let moon = document.getElementById("moon")
+    let mountain = document.getElementById("mountain")
+    let road = document.getElementById("road")
+    let text1 = document.getElementById("text1")
+
+    window.addEventListener('scroll', function(){
+
+    var value = window.scrollY;
+
+    bg.style.top = value * 0.5 + 'px';
+    moon.style.left = -value * 0.5 + 'px';
+    mountain.style.top = -value * 0.15 + 'px';
+    road.style.top = value * 0.15 + 'px';
+    text.style.top = value * 1 + 'px';
+    
+})
  
 });
